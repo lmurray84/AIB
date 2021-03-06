3a1a19c19589cf4cf6574f1ee2b06fa6469f2942
lmurray84 <52921634+lmurray84@users.noreply.github.com>
2021-03-06 15:28:23 +0000

    Create Install_M365_Apps.ps1

---
+++ Apps/Install_M365_Apps.ps1
@@ -0,0 +1,18 @@
+$softwareRepo = "C:\Temp\Software\M365"
+
+# Create M365 software repository
+Write-Host "Azure Image Builder: Creating Microsoft 365 Apps for Enterprise software repository"
+New-Item -ItemType Directory -Path $softwareRepo -Force -ErrorAction SilentlyContinue
+
+# Download latest Office Deployment Tool
+Write-Host "Azure Image Builder: Downloading the latest Office Deployment Tool software"
+Invoke-WebRequest -Uri https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_13328-20420.exe -OutFile "$softwareRepo\ODT.exe"
+
+# Extract Office Deployment Tool software
+Write-Host "Azure Image Builder: Extracting the Office Deployment Tool software"
+Invoke-Expression -Command "$softwareRepo\ODT.exe /extract:$softwareRepo\M365 /quiet /norestart"
+Start-Sleep 5
+
+# Download XML configuration file
+Write-Host "Azure Image Builder: Downloading the XML configuration file"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/lmurray84/AIB/main/M365_Apps.xml -OutFile "$softwareRepo\M365\M365_Apps.xml"
