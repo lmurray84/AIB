017fc06d05e03b7dcd4dc57e9ee1870734ded698
lmurray84 <52921634+lmurray84@users.noreply.github.com>
2021-03-06 11:46:03 +0000

    Update Add_Language_en-GB.ps1

--- Languages/Add_Language_en-GB.ps1
+++ Languages/Add_Language_en-GB.ps1
@@ -1,4 +1,10 @@
-# Install language pack
+# Download language packs
+Write-Host
+Invoke-WebRequest -Uri https://github.com/lmurray84/AIB/blob/main/LanguageExperiencePack.en-GB.Neutral.appx?raw=true -OutFile "$softwareRepo\LanguageExperiencePack.en-GB.Neutral.appx"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/lmurray84/AIB/main/License.xml -OutFile "$softwareRepo\License.xml"
+Invoke-WebRequest -Uri https://github.com/lmurray84/AIB/blob/main/Microsoft-Windows-Client-Language-Pack_x64_en-gb.cab?raw=true -OutFile "$softwareRepo\Microsoft-Windows-Client-Language-Pack_x64_en-gb.cab"
+
+# Install language packs
 Add-AppProvisionedPackage -Online -PackagePath C:\Languages\en-GB\LanguageExperiencePack.en-GB.Neutral.appx -LicensePath C:\Languages\en-GB\License.xml
 Add-WindowsPackage -Online -PackagePath C:\Languages\en-GB\Microsoft-Windows-Client-Language-Pack_x64_en-gb.cab
 
