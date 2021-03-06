a65bcaf8c4e755e52faeb2b0b1421ee5ff4f4467
lmurray84 <52921634+lmurray84@users.noreply.github.com>
2021-03-06 11:42:32 +0000

    Delete Add_Language_en-GB.ps1

--- Languages/en-GB/Add_Language_en-GB.ps1
+++
@@ -1,8 +0,0 @@
-# Install language pack
-Add-AppProvisionedPackage -Online -PackagePath C:\Languages\en-GB\LanguageExperiencePack.en-GB.Neutral.appx -LicensePath C:\Languages\en-GB\License.xml
-Add-WindowsPackage -Online -PackagePath C:\Languages\en-GB\Microsoft-Windows-Client-Language-Pack_x64_en-gb.cab
-
-# Set language pack
-$LanguageList = Get-WinUserLanguageList
-$LanguageList.Add("en-GB")
-Set-WinUserLanguageList $LanguageList -Force
