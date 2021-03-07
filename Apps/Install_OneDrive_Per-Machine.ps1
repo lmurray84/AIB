8e9784d169ecd7f15c17845192231d51af4d1a18
lmurray84 <52921634+lmurray84@users.noreply.github.com>
2021-03-07 13:44:11 +0000

    Update Install_OneDrive_Per-Machine.ps1

--- Apps/Install_OneDrive_Per-Machine.ps1
+++ Apps/Install_OneDrive_Per-Machine.ps1
@@ -7,6 +7,7 @@ New-Item -ItemType Directory -Path $softwareRepo -Force -ErrorAction SilentlyCon
 # Download OneDrive for Business
 Write-Host "Azure Image Builder: Downloading OneDrive for Business latest software"
 Invoke-WebRequest -Uri "https://aka.ms/OneDriveWVD-Installer" -OutFile "$softwareRepo\OneDriveSetup.exe"
+Start-Sleep 5
 
 # Install OneDrive for Business
 Write-Host "Azure Image Builder: Installing OneDrive for Business"
