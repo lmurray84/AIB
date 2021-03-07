fb250bb7d3c642d6ca182472e79160f55c2bf2e0
lmurray84 <52921634+lmurray84@users.noreply.github.com>
2021-03-07 13:59:37 +0000

    Update Install_OneDrive_Per-Machine.ps1

--- Apps/Install_OneDrive_Per-Machine.ps1
+++ Apps/Install_OneDrive_Per-Machine.ps1
@@ -10,5 +10,5 @@ Invoke-WebRequest -Uri "https://aka.ms/OneDriveWVD-Installer" -OutFile "$softwar
 
 # Install OneDrive for Business
 Write-Host "Azure Image Builder: Installing OneDrive for Business"
-Start-Process -FilePath "$softwareRepo\OneDriveSetup.exe" -ArgumentList "/allusers /silent /norestart" -Wait
+Start-Process -FilePath "$softwareRepo\OneDriveSetup.exe" -ArgumentList "/allusers" -Wait
 Write-Host "Azure Image Builder: Completed OneDrive for Business installation"
