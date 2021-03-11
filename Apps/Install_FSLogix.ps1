2876afac44f89704f965b73b578e01ba28d9f7f0
lmurray84 <52921634+lmurray84@users.noreply.github.com>
2021-03-11 13:19:54 +0000

    Update Install_FSLogix.ps1

--- Apps/Install_FSLogix.ps1
+++ Apps/Install_FSLogix.ps1
@@ -5,6 +5,7 @@ Write-Host "Azure Image Builder: Creating FSLogix software repository"
 New-Item -ItemType Directory -Path $softwareRepo -Force -ErrorAction SilentlyContinue
 
 # Download FSLogix Apps
+$progressPreference = 'SilentlyContinue'
 Write-Host "Azure Image Builder: Downloading FSLogix Apps latest software"
 Invoke-WebRequest -Uri "https://aka.ms/fslogix_download" -OutFile "$softwareRepo\FSLogix.zip"
 
