88d1e99583b314fe53c7f1eae6715de8c0de00eb
lmurray84 <52921634+lmurray84@users.noreply.github.com>
2021-03-11 13:19:38 +0000

    Update Install_OneDrive_Per-Machine.ps1

--- Apps/Install_OneDrive_Per-Machine.ps1
+++ Apps/Install_OneDrive_Per-Machine.ps1
@@ -5,10 +5,11 @@ Write-Host "Azure Image Builder: Creating OneDrive for Business software reposit
 New-Item -ItemType Directory -Path $softwareRepo -Force -ErrorAction SilentlyContinue
 
 # Download OneDrive for Business
+$progressPreference = 'SilentlyContinue'
 Write-Host "Azure Image Builder: Downloading OneDrive for Business latest software"
 Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?linkid=844652" -OutFile "$softwareRepo\OneDriveSetup.exe"
 
 # Install OneDrive for Business
 Write-Host "Azure Image Builder: Installing OneDrive for Business"
-Start-Process -FilePath "$softwareRepo\OneDriveSetup.exe" -ArgumentList "/allusers" -Wait
+Start-Process -FilePath "$softwareRepo\OneDriveSetup.exe" -ArgumentList "/allusers /silent"
 Write-Host "Azure Image Builder: Completed OneDrive for Business installation"
